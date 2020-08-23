61b30aa69f292d74fba63231532fd0b7431937e7
Jerry777777 <491818175@qq.com>
2020-08-23 23:24:37 +0800

    [xiawenhao] feat: add the error of username and password

---
+++ app/src/main/java/com/xiawenhao/todolist/Person.java
@@ -0,0 +1,41 @@
+package com.xiawenhao.todolist;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.PrimaryKey;
+
+public class Person {
+    @NonNull
+    @PrimaryKey
+    private int id;
+
+    @ColumnInfo
+    private String name;
+
+    @ColumnInfo
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
