707901eea5eef726ff2a65503708e7385314cf96
Jerry777777 <491818175@qq.com>
2020-08-26 23:53:13 +0800

    [xiawenhao] fix：add back button and checkbox

--- app/src/main/java/com/xiawenhao/todolist/AddItemActivity.java
+++ app/src/main/java/com/xiawenhao/todolist/AddItemActivity.java
@@ -1,4 +1,23 @@
 package com.xiawenhao.todolist;
 
-public class CreatItemActivity {
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddItemActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creat);
+
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            (actionBar).hide();
+        }
+    }
 }
--- app/src/main/java/com/xiawenhao/todolist/ListActivity.java
+++ app/src/main/java/com/xiawenhao/todolist/ListActivity.java
@@ -1,16 +1,21 @@
 package com.xiawenhao.todolist;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class ListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        ButterKnife.bind(this);
 
         View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
@@ -19,5 +24,12 @@ public class ListActivity extends AppCompatActivity {
         if (actionBar != null) {
             (actionBar).hide();
         }
+    }
+
+    @OnClick(R.id.add_button)
+    public void clickAddItem() {
+        Intent intent = new Intent(ListActivity.this, AddItemActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
