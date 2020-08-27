c16eb4bcad7fc12b1cf423478de94be3937eb0b1
Jerry777777 <491818175@qq.com>
2020-08-28 02:10:40 +0800

    [xiawenhao] feat: click date button can show calenderList

--- app/src/main/java/com/xiawenhao/todolist/AddItemActivity.java
+++ app/src/main/java/com/xiawenhao/todolist/AddItemActivity.java
@@ -11,11 +11,14 @@ import android.widget.ImageButton;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.time.LocalDate;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class AddItemActivity extends AppCompatActivity {
+    private static final int MONTH_ADD_NUMBER = 1;
     @BindView(R.id.select_date)
     Button selectDate;
     @BindView(R.id.calender_list)
@@ -27,11 +30,17 @@ public class AddItemActivity extends AppCompatActivity {
     @BindView(R.id.add_item)
     ImageButton addItem;
 
+    private boolean showCalender = false;
+    private LocalDate showLocalCalender;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creat);
         ButterKnife.bind(this);
+        calendarList.setOnDateChangeListener((view, year, month, dayOfMonth) -> {
+            showLocalCalender = LocalDate.of(year, month + MONTH_ADD_NUMBER, dayOfMonth);
+        });
 
         View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
@@ -39,6 +48,28 @@ public class AddItemActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             (actionBar).hide();
+        }
+    }
+
+    @OnClick({R.id.select_date, R.id.add_item, R.id.back_button})
+    void btnClick(View view) {
+        switch (view.getId()) {
+            case R.id.back_button:
+                Intent intent = new Intent(AddItemActivity.this, ListActivity.class);
+                startActivity(intent);
+                this.finish();
+                break;
+            case R.id.select_date:
+                if (!showCalender) {
+                    calendarList.setVisibility(View.VISIBLE);
+                    showCalender = true;
+                } else {
+                    calendarList.setVisibility(View.GONE);
+                    showCalender = false;
+                }
+                break;
+            case R.id.add_item:
+                break;
         }
     }
 }
